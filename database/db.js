abb7b0782fd9c101a5d8f95e04365795dce86efa
Brandon Danis <brandon@spottedu.co>
2016-02-25 15:16:07 -0500

    Changed from mySQL to psql

--- database/db.js
+++ database/db.js
@@ -1,22 +1,19 @@
 DROP TABLE movies;
 CREATE TABLE movies (
-  id int(11) NOT NULL auto_increment,
+  id serial PRIMARY KEY,
   overview text NOT NULL,
   release_date varchar(255) NOT NULL,
-  runtime int(11) NOT NULL,
-  genre_id int(11),
+  runtime integer NOT NULL,
+  genre_id integer REFERENCES genres,
   poster varchar(255) NOT NULL,
-  rating int(11) NOT NULL,
-  title text NOT NULL,
-  PRIMARY KEY  ('id'),
-  FOREIGN KEY ('genre_id') REFERENCES 'genres'
+  rating integer NOT NULL,
+  title text NOT NULL
 );
 
 DROP TABLE genres;
 CREATE TABLE genres (
-  id int(11) NOT NULL auto_increment,
-  name varchar(255) NOT NULL,
-  PRIMARY KEY  ('id')
+  id serial PRIMARY KEY,
+  name varchar(255) NOT NULL
 );
 
 INSERT INTO genres (name) VALUES
