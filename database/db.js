819b755a76fce50104486d211130f151a0c684c5
Brandon Danis <brandon@spottedu.co>
2016-02-25 14:59:27 -0500

    Changed some attributes

--- database/db.js
+++ database/db.js
@@ -1,23 +1,22 @@
 DROP TABLE 'movies';
 CREATE TABLE 'movies' (
   'id' int(11) NOT NULL auto_increment,
-  'original_title' text NOT NULL,
   'overview' text NOT NULL,
   'release_date' varchar(255) NOT NULL,
   'runtime' int(11) NOT NULL,
+  'genre_id' int(11),
   'poster' varchar(255) NOT NULL,
   'rating' int(11) NOT NULL,
   'title' text NOT NULL,
-  'url' varchar(255) NOT NULL,
   'vote_average' float NOT NULL,
   'vote_count' int(11) NOT NULL,
-  PRIMARY KEY  ('id')
+  PRIMARY KEY  ('id'),
+  FOREIGN KEY ('genre_id') REFERENCES 'genres'
 );
 
 DROP TABLE 'genres';
 CREATE TABLE 'genres' (
   'id' int(11) NOT NULL auto_increment,
-  'tmdb_id' int(11) NOT NULL,
   'name' varchar(255) NOT NULL,
   PRIMARY KEY  ('id')
 );
