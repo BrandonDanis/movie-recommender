d25c874a11f435671afb92e059fbb85380a1f720
Rushil Perera <pererarushil1940@gmail.com>
2016-03-05 04:29:00 -0500

    Remove unnecessary class

--- app/js/movie.js
+++ app/js/movie.js
@@ -18,7 +18,7 @@ loadMovie = function (idTitle) {
 
 generateMovie = function (movie) {
     var movieHTML = "<div class=\"row\"><p style=\"float: left;\"><img id=\"poster\" " +
-        "src=\"https://image.tmdb.org/t/p/w185~IMGURL~\"/></p><p class=\"col-md-8\"><h2>~MOVIETITLE~ " +
+        "src=\"https://image.tmdb.org/t/p/w185~IMGURL~\"/></p><p><h2>~MOVIETITLE~ " +
         "(~MOVIERELEASEYEAR~)</h2><br/><b>Directed by:</b> Tim Miller<br/><b>Genres:</b> Adventure, Action, Comedy<br/>" +
         "<span class=\"rating\"><span class=\"star\"><i class=\"fa fa-star-o\"></i></span><span class=\"star\">" +
         "<i class=\"fa fa-star-o\"></i></span><span class=\"star\"><i class=\"fa fa-star-o\"></i></span>" +
