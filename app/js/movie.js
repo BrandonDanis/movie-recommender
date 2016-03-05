3591f4656d7a8997a6779b8cefcbf7dc0351723e
Rushil Perera <pererarushil1940@gmail.com>
2016-03-05 15:53:13 -0500

    Changed specific movie route to return genres, director and cast as well

--- app/js/movie.js
+++ app/js/movie.js
@@ -19,18 +19,15 @@ loadMovie = function (idTitle) {
 generateMovie = function (movie) {
     var movieHTML = "<div class=\"row\"><p style=\"float: left;\"><img id=\"poster\" " +
         "src=\"https://image.tmdb.org/t/p/w185~IMGURL~\"/></p><p><h2>~MOVIETITLE~ " +
-        "(~MOVIERELEASEYEAR~)</h2><br/><b>Directed by:</b> Tim Miller<br/><b>Genres:</b> Adventure, Action, Comedy<br/>" +
+        "(~MOVIERELEASEYEAR~)</h2><br/><b>Directed by:</b> ~MOVIEDIRECTOR~<br/><b>Genres:</b> ~MOVIEGENRES~<br/>" +
         "<span class=\"rating\"><span class=\"star\"><i class=\"fa fa-star-o\"></i></span><span class=\"star\">" +
         "<i class=\"fa fa-star-o\"></i></span><span class=\"star\"><i class=\"fa fa-star-o\"></i></span>" +
         "<span class=\"star\"><i class=\"fa fa-star-o\"></i></span><span class=\"star\"><i class=\"fa fa-star-o\"></i>" +
         "</span></span></p></div><div class=\"row col-md-12\"><button style=\"margin-left: 15px\">Trailer</button>" +
         "</div><div class=\"row col-md-12\"><div><h3>Description:</h3>~MOVIEDESCRIPTION~</div></div>";
 
-    movieHTML = movieHTML.replace('~IMGURL~', movie.poster);
-    movieHTML = movieHTML.replace('~MOVIETITLE~', movie.title);
-    movieHTML = movieHTML.replace('~MOVIERELEASEYEAR~', movie.release_date.split('-')[0]);
-    movieHTML = movieHTML.replace('~MOVIEDESCRIPTION~', movie.overview);
-
-    return movieHTML;
+    return movieHTML.replace('~IMGURL~', movie.poster).replace('~MOVIETITLE~', movie.title)
+        .replace('~MOVIERELEASEYEAR~', movie.release_date.split('-')[0]).replace('~MOVIEDESCRIPTION~', movie.overview)
+        .replace('~MOVIEGENRES~', movie.genres.join(', ')).replace('~MOVIEDIRECTOR~', movie.director.name);
 };
 
