9d269a4a5b7cd33668c82926aa3aac8e5b224f28
Brandon Danis <brandon@spottedu.co>
2016-03-15 17:16:27 -0400

    Sorting by release data

--- app/js/index.js
+++ app/js/index.js
@@ -121,6 +121,29 @@ loadMoviesFromAZ = function() {
 
 };
 
+loadMoviesByRelease = function() {
+
+    api.getAllMovieByRelease(function(res) {
+
+        var moviesArray = res['movies'];
+
+        if (res['status'] == 200) {
+
+            $('#movieContainer').empty();
+
+            for (var i = 0; i < (200 || moviesArray.length); i++) {
+                $('#movieContainer').append(generateMovieDiv(moviesArray[i]['id'], moviesArray[i]['title'], moviesArray[i]['poster']));
+            }
+
+        } else {
+            console.log('Error getting movies');
+        }
+
+    });
+
+};
+
+
 generateMovieDiv = function (movieId, movieTitle, posterUrl) {
 
     var divTemplate = '<div class="movieBox"><a href="./movie?id=~MOVIEID~"><div class="imageContainer"><img src="https://image.tmdb.org/t/p/w185~IMGURL~" width="185" height="278"/></div><div class="movieInfo"><h4>~MOVIETITLE~</h4></div></a></div>';
