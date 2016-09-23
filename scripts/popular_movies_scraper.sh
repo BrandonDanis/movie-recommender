ea29493563e9b257b446ee0efa8adb80ac6a2acc
Rushil Perera <pererarushil1940@gmail.com>
2016-09-23 17:12:36 -0400

    Combined some Python scripts

--- scripts/popular_movies_scraper.sh
+++ scripts/popular_movies_scraper.sh
@@ -1,7 +1,4 @@
-for i in 11 12 13 14 15 16 17 18 19 20
-do
-	python scraper.py $(($(($i - 1)) * 10)) $i
-	python movie_runtime_loader.py $i
-	python fix_json_files.py $i
-	python runtime_merge.py $i
-done
+python populate.py $1 $2 $3 $4
+python populateCrew.py $1 $2 $3 $4
+python retrieveTrailers.py $1 $2 $3 $4
+python get_bio.py $1 $2 $3 $4
--- scripts/populate.py
+++ scripts/populate.py
@@ -1,10 +1,44 @@
-import json
-import psycopg2
 import sys
+
+import psycopg2
+import requests
+import time
+
+
+def scrape(init):
+    movies = []
+    i = init
+    while i < (init + 10):
+        movie_load = requests.get(
+            'https://api.themoviedb.org/3/movie/popular?api_key=476bbe4282fb66cfbd54f6da2d3d28fe&page=' + str(i))
+        movie_json_array = movie_load.json()['results']
+        print(movie_json_array)
+        for movie_json in movie_json_array:
+            id = movie_json['id']
+            run = True
+            while run:
+                movie_runtime_json = requests.get(
+                    'https://api.themoviedb.org/3/movie/{0}?api_key=476bbe4282fb66cfbd54f6da2d3d28fe'.format(id)).json()
+                if 'runtime' not in movie_runtime_json:
+                    print('Waiting 5 seconds because going over request limit...')
+                    time.sleep(5)
+                    run = True
+                    print('Finished waiting 5 seconds...')
+                else:
+                    run = False
+            movie = {'id': id, 'overview': movie_json['overview'], 'release_date': movie_json['release_date'],
+                     'runtime': movie_runtime_json['runtime'], 'poster': movie_json['poster_path'],
+                     'title': movie_json['title'], 'genre_ids': movie_json['genre_ids']}
+            movies.append(movie)
+            print(movie)
+        i += 1
+    return movies
+
 
 args = sys.argv
 database = args[1]
 host = args[2]
+print "Connecting to DB"
 if len(args) > 3:
     username = args[3]
     password = args[4]
@@ -12,6 +46,7 @@ if len(args) > 3:
 else:
     connection = psycopg2.connect(database=database, host=host)
 db = connection.cursor()
+print "Connected to DB"
 
 genresDict = {
     28: "Action",
@@ -36,57 +71,52 @@ genresDict = {
     37: "Western"
 }
 
-i = 11
-while i < 21:
-    print i
-    file_name = 'popular_movies_runtimes_merged_' + str(i) + '.json'
-    f = open(file_name, 'r').read()
-    movie_data = json.loads(f)
+movie_data = []
+movie_data.extend(scrape(1))
+movie_data.extend(scrape(11))
 
-    for movie_json in movie_data:
-        try:
-            movie = {
-                'overview': movie_json['overview'],
-                'release_date': movie_json['release_date'],
-                'runtime': movie_json['runtime'],
-                'poster': movie_json['poster'],
-                'rating': 0,
-                'moviedb_id': movie_json['id'],
-                'title': movie_json['title'],
-                'genre_ids': movie_json['genre_ids']
+for movie_json in movie_data:
+    try:
+        movie = {
+            'overview': movie_json['overview'],
+            'release_date': movie_json['release_date'],
+            'runtime': movie_json['runtime'],
+            'poster': movie_json['poster'],
+            'rating': 0,
+            'moviedb_id': movie_json['id'],
+            'title': movie_json['title'],
+            'genre_ids': movie_json['genre_ids']
+        }
+
+        db.execute(
+            'INSERT INTO movies (overview, release_date, runtime, poster, moviedb_id, title) VALUES '
+            '(%s, %s, %s, %s, %s, %s) RETURNING *', (
+                movie['overview'], movie['release_date'], movie['runtime'], movie['poster'], movie['moviedb_id'],
+                movie['title']))
+        connection.commit()
+
+        row = db.fetchone()
+        movie['id'] = row[0]
+        print(row[7] + ' added | id: ' + str(row[0]))
+
+        for genreID in movie['genre_ids']:
+            db.execute('SELECT id FROM genres WHERE name=%s', (genresDict[genreID],))
+            row = db.fetchone()
+
+            genre_movie = {
+                'movie_id': movie['id'],
+                'genre_id': row[0]
             }
 
-            db.execute(
-                'INSERT INTO movies (overview, release_date, runtime, poster, moviedb_id, title) VALUES '
-                '(%s, %s, %s, %s, %s, %s) RETURNING *', (
-                    movie['overview'], movie['release_date'], movie['runtime'], movie['poster'], movie['moviedb_id'],
-                    movie['title']))
+            db.execute('INSERT INTO movies_genres VALUES (%s, %s)',
+                       (genre_movie['movie_id'], genre_movie['genre_id']))
             connection.commit()
-
-            row = db.fetchone()
-            movie['id'] = row[0]
-            print(row[7] + ' added | id: ' + str(row[0]))
-
-            for genreID in movie['genre_ids']:
-                db.execute('SELECT id FROM genres WHERE name=%s', (genresDict[genreID],))
-                row = db.fetchone()
-
-                genre_movie = {
-                    'movie_id': movie['id'],
-                    'genre_id': row[0]
-                }
-
-                db.execute('INSERT INTO movies_genres VALUES (%s, %s)',
-                           (genre_movie['movie_id'], genre_movie['genre_id']))
-                connection.commit()
-                print(movie['title'] + ' --> ' + str(genreID))
-        except psycopg2.IntegrityError as e:
-            print e
-            connection.rollback()
-            print movie['title'] + ' might already exist...row had to be rollbacked'
-        except psycopg2.InternalError as e2:
-            print e2
-            connection.rollback()
-            print movie['title'] + ' failed and had to be rollbacked...'
-
-    i += 1
+            print(movie['title'] + ' --> ' + str(genreID))
+    except psycopg2.IntegrityError as e:
+        print e
+        connection.rollback()
+        print movie['title'] + ' might already exist...row had to be rollbacked'
+    except psycopg2.InternalError as e2:
+        print e2
+        connection.rollback()
+        print movie['title'] + ' failed and had to be rollbacked...'
